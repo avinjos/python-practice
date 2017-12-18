58dbaf3c9cc23b4cbdbe3631ed7d883a3aba3d2d
root <root@ip-172-31-41-83.us-east-2.compute.internal>
2017-12-18 02:06:18 +0000

    practice for the day Dec 17th

---
+++ variables.py
@@ -0,0 +1,27 @@
+parrot  = "Norweign Parrot"
+## This is practice program
+print (parrot[-1])
+print (parrot)
+print (parrot[3])
+print (parrot[0:6])
+print (parrot[:6])
+print (parrot[6:])
+print (parrot[0:6:2])
+print (parrot[0:6:3])
+
+
+numbers = "1, 2, 3, 4, 5, 6"
+print(numbers)
+print (numbers[0::2])
+
+string1 = "he's"
+string2 = "probably"
+
+print (string1 + string2)
+print ("He 's "  " probably " " pining ")
+print ("Hello " *5)
+today = "friday"
+print ("day" in today)
+print ("fri" in today)
+print ("thu" in today)
+print ("parrot" in "fjrok")
